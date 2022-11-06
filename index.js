8447d7e6466d7bba8b511438b05da67334ace481
Katie <katiefaybehrmann@gmail.com>
2022-11-06 16:39:21 -0700

    commit tree 7f9948bfcd8325c4a6f32758c6356a013c0e2702

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array) {
+    let won = array.find((item) => item.result === "W") 
+        if (won){
+            won = won.year;
+        }
+        return won;
+}
+
+
